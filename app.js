0576ab90df68e213d827c520a52d90e6e7ba530b
Ferdjani Zakaria <ferdjani.abdelkaderzakaria@gmail.com>
2022-08-25 14:31:41 +0100

    add condition of mouseDown to call fct  changeColor

--- app.js
+++ app.js
@@ -2,21 +2,55 @@ const canvas = document.querySelector('.canvas');
 const resetBtn = document.querySelector('.btn');
 const pixelRange = document.querySelector('.numPixel');
 
-const div = document.createElement('div');
+let mouseDown = false;
 
-// window.onload = resetCanvas(16)
+
 
 const updateTextCanvasSize =(val)=> {
     document.querySelector('#canvasSize').value=`${val}x${val}`; 
 }
 
-
-const resetCanvas =()=> {
-    const pixel = document.querySelectorAll('.canvas div');
+const changeColor =(div)=>{
+    div.style.backgroundColor = "black";
 }
 
 
-resetBtn.addEventListener('click', resetCanvas)
+const resetCanvas =(num)=> {
+    canvas.innerHTML = "";
+    canvas.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    for(i=0; i<num*num; i++){
+        const div = document.createElement('div');
+        canvas.appendChild(div);
+        div.addEventListener('mouseover', (e)=>{
+            if(mouseDown){
+                changeColor(e.target);
+            }
+            c
+        })
+    }
+    
+}
+
+
+resetBtn.addEventListener('click',(e)=>{ 
+    let num = pixelRange.value;
+    resetCanvas(num);
+});
 pixelRange.addEventListener('input', (e)=>{
     updateTextCanvasSize(e.target.value);
 })
+
+pixelRange.addEventListener('change', (e)=>{
+    resetCanvas(e.target.value);
+})
+
+// Check for mouse if is Down
+document.addEventListener('mousedown', function(){
+    mouseDown = true;
+});
+document.addEventListener('mouseup', function(){
+    mouseDown = false;
+});
+
+
+window.onload = resetCanvas(16)
